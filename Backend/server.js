f549cac9e5247d2a3650d3f15cffbb671360ad70
Prasanth-1011 <112630396+Prasanth-1011@users.noreply.github.com>
2025-10-30 12:07:29 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -12,6 +12,7 @@ const app = express();
 
 app.use(cors({
     origin: 'http://localhost:5173',
+    https://excel-data-analytics-seven.vercel.app/,
     credentials: true
 }));
 app.use(express.json());
@@ -41,4 +42,5 @@ app.use('/api/visualizations', visualizationRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+
 });
